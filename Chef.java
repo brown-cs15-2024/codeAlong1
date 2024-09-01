adbedfef89b751ba3efbc5135aae7d34a0797d3d
Brendan Rathier <brendan_rathier@brown.edu>
2024-09-01 16:53:29 -0400

    Finished solution code for codeAlong1

--- Chef.java
+++ Chef.java
@@ -2,10 +2,19 @@ package codeAlong1;
 
 public class Chef {
 
+    //This is sol. Make the solution.
     public Chef() {
     }
 
     public void makePizza(String topping) {
         System.out.println("Making pizza with " + topping);
+    }
+
+    public void cutPizza(int numPieces){
+        System.out.println("Cutting up " + numPieces + " slices!");
+    }
+
+    public int howManySlices(int numPizza, int numSlices){
+        return numPizza * numSlices;
     }
 }
--- Restaurant.java
+++ Restaurant.java
@@ -3,11 +3,18 @@ package codeAlong1;
 public class Restaurant {
 
     public Restaurant(Chef myChef) {
+        this.openRestaurant();
+        this.prepareOrders(myChef);
+    }
+
+    public void openRestaurant(){
+        System.out.println("Open for business!");
     }
 
 
     public void prepareOrders(Chef myChef) {
         System.out.println("Taking orders");
         myChef.makePizza("pepperoni");
+        myChef.cutPizza(8);
     }
 }
